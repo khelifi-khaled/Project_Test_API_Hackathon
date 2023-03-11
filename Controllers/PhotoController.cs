56be0f0078a63248fab8d9a3396e2b9838c72044
Khaled KHELIFI <khaled.khelifi@iramps.email>
2023-03-11 17:12:49 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/PhotoController.cs
@@ -0,0 +1,80 @@
+﻿
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Project_Test_API_Hackathon.Entities;
+using System;
+using System.Reflection;
+
+
+namespace Project_Test_API_Hackathon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+
+        public PhotoController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+
+
+
+        // POST api/<PhotoController>
+        [HttpPost]
+        public IActionResult Post([FromForm] Photo modelPhoto)
+        {
+
+            try
+            {
+
+                if (modelPhoto == null)
+                {
+                    return Content("Invalid Submission!");
+                }
+
+                if (modelPhoto.PhotoFile.FileName == null || modelPhoto.PhotoFile.FileName.Length == 0)
+                {
+                    return Content("File not selected");
+                }
+
+
+                if (string.IsNullOrWhiteSpace(_env.WebRootPath))
+                {
+                    _env.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+                }
+
+                var path = Path.Combine(_env.WebRootPath, "Images/", modelPhoto.PhotoFile.FileName);
+
+
+                using (FileStream stream = new (path, FileMode.Create))
+                {
+                    modelPhoto.PhotoFile.CopyTo(stream);
+                    stream.Close();
+                }
+
+
+
+
+
+
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Entities/Photo.cs
@@ -0,0 +1,14 @@
+﻿namespace Project_Test_API_Hackathon.Entities
+{
+#nullable disable
+
+    public class Photo
+    {
+
+        public string PhotoName { get; set; }
+
+        public IFormFile PhotoFile { get; set; }
+
+
+    }//end class 
+}//end namespace 
